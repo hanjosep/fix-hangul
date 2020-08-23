0929436f36e54b524f6e90848f3aefc20edf8296
Josep Han <hanjosep@gmail.com>
2020-08-23 02:07:47 -0400

    try/exception added

--- Fix Korean encode.py
+++ Fix Korean encode.py
@@ -11,22 +11,30 @@ text ="C:\\Users\\Josep Han\\Music\\¼ÒÇâ - 2. ¾Æºü¿ÍÀÇ ¿¹¹è.mp3"
 # print(os.listdir("C:\\Users\\Josep Han\\Music\\"))
 # prints list of broken filenames within folder
 
-brokenlist=os.listdir("C:\\Users\\Josep Han\\Music\\")
-print(brokenlist)
+# brokenlist=os.listdir("C:\\Users\\Josep Han\\Music\\") #windows ver
+brokenlist=os.listdir("/home/hanjosep/Music/") #linux ver
+
+# print(brokenlist)
 fixedlist = []
 
 for item in brokenlist:
-    fixedlist.append(item.encode('windows-1252', "ignore").decode('korean', 'replace'))
+    try:
+        fixedlist.append(item.encode('windows-1252').decode('korean', 'replace'))
+    except: #if above fails, then assume the given file is already in Korean encode.
+        fixedlist.append(item)
 
 
 # print(fixedlist)
 parent_path = "C:\\Users\\Josep Han\\Music\\"
-# fix the broken file names into the properly encoded filenames.
-# for broken in brokenlist:
-#     for fixed in fixedlist:
-#         os.rename(parent_path + broken, parent_path+fixed)
+parent_path = "/home/hanjosep/Music/" #linux ver
 res = {brokenlist[i]: fixedlist[i] for i in range(len(fixedlist))} 
-# print(res)
-# print(len(fixedlist))
+print(res)
+print(len(fixedlist))
 # print((brokenlist))
 
+
+# fix the broken file names into the properly encoded filenames.
+for broken,fixed in res.items():
+    # print(broken)
+
+    os.rename(parent_path + broken, parent_path+fixed)
